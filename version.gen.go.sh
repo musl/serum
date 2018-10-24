3b8a412ea1162ce9474d9c63ba0b62e33bc8fa39
Mike Hix <mhix@newrelic.com>
2018-10-24 16:52:41 -0700

    Add comments to generated code

--- version.gen.go.sh
+++ version.gen.go.sh
@@ -3,7 +3,10 @@ cat <<EOF > `basename ${0/\.sh/}`
 package serum
 
 const (
+	// Version is the semantic version string for this library.
 	Version = "`git describe --abbrev=0 --tags 2>/dev/null || echo 0.0.0`"
+
+	// Revision is the DVCS commit ID for this code.
 	Revision = "`git rev-parse HEAD || echo 0000000000000000000000000000000000000000`"
 )
 EOF
