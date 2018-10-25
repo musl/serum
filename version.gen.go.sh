2758f9172e750c9561411b503042c259c7e9eb46
Mike Hix <mhix@newrelic.com>
2018-10-24 18:42:53 -0700

    Clean up the template a tad

--- version.gen.go.sh
+++ version.gen.go.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
+version=`git describe --abbrev=0 --tags 2>/dev/null || echo 0.0.0`
+revision=`git rev-parse HEAD || echo 0000000000000000000000000000000000000000`
+
 cat <<EOF > `basename ${0/\.sh/}`
 package serum
 
 const (
 	// Version is the semantic version string for this library.
-	Version = "`git describe --abbrev=0 --tags 2>/dev/null || echo 0.0.0`"
+	Version = "$version"
 
 	// Revision is the DVCS commit ID for this code.
-	Revision = "`git rev-parse HEAD || echo 0000000000000000000000000000000000000000`"
+	Revision = "$revision"
 )
 EOF
