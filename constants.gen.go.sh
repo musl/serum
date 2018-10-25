0e42ac0a089158caa8aafda623a96ebc1b051ea0
Mike Hix <mhix@newrelic.com>
2018-10-25 00:54:29 -0700

    Add authors constant

--- constants.gen.go.sh
+++ constants.gen.go.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
+
 version=`git describe --abbrev=0 --tags 2>/dev/null || echo 0.0.0`
-revision=`git rev-parse HEAD || echo 0000000000000000000000000000000000000000`
+revision=`git rev-parse HEAD 2>/dev/null || echo 0000000000000000000000000000000000000000`
 
 cat <<EOF > `basename ${0/\.sh/}`
 package serum
@@ -11,5 +12,10 @@ const (
 
 	// Revision is the DVCS commit ID for this code.
 	Revision = "$revision"
+
+	// Contributors is a list of all of the contributors to this code.
+	Authors = \`
+		Mike Hix <m@hix.io>
+	\`
 )
 EOF
